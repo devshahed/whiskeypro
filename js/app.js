8aa3729714ac4fc12e809d00071810ab98609b98
root <root@lazyCoder.shahed>
2021-12-17 04:10:56 +0600

    responsive

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
 let sidebar = document.getElementById("sidebar");
+let header = document.getElementsByTagName("HEADER")[0];
 
 function showSidebar(){
     if(sidebar.classList.contains("sidebar")){
         sidebar.classList.remove("sidebar");
+        header.classList.remove("blackbg");
     }else{
         sidebar.classList.add("sidebar");
+        header.classList.add("blackbg");
     }
 }
